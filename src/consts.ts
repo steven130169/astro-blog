e5cef2d399981c9264e66bdcb00cbb7757914e50
prakhardixit9793 <prakhardixit9793@gmail.com>
2026-05-16 06:36:56 +0530

    feat: embed Cal.com booking widget and add header nav link

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@ export const AUTHOR_NAME = 'Steven Cheng (鄭棋文)';
 export const AUTHOR_EMAIL = 'steven@chiwencheng.com';
 export const GITHUB_URL = 'https://github.chiwencheng.com';
 export const THREADS_URL = 'https://www.threads.com/@steven.tech.lab';
+export const CALCOM_URL = 'https://cal.com/steven.cheng';
